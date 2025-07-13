d25b26b715b9d4da715a1b9c8e683802e81fed3e
Maczz09 <u22201741@utp.edu.pe>
2025-07-13 17:39:31 -0500

    .env

--- manage.py
+++ manage.py
@@ -2,7 +2,8 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from dotenv import load_dotenv
+load_dotenv() 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cv_uploader.settings')
     try:
